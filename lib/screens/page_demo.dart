bbd378cb5a715a2455057f397e3d29f64441f994
aman-abiy <amexabiy@gmail.com>
2022-01-24 12:53:17 +0300

    opted for animated opacity

--- lib/screens/page_demo.dart
+++ lib/screens/page_demo.dart
@@ -11,90 +11,86 @@ class _PageDemoState extends State<PageDemo> with SingleTickerProviderStateMixin
   int counter = 1;
   bool isTapped = false;
   Color onTapColor = Colors.grey;
-  late AnimationController animationController;
-  var background;
+
+  double _currentOpacity =  0.0;
   
 
 @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    animationController = AnimationController(
-      duration: const Duration(milliseconds: 100),
-      vsync: this,
-    );
-
-    background = ColorTween(
-    begin: Colors.grey.shade200,
-    end: Colors.blue,
-  ).animate(animationController)
-    ..addListener(() {
-      setState(() {});
-    });
+    
   }
   
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animationController, 
-      builder: (context, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Page Demo'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Demo'),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Text(counter.toString(),
+              style: TextStyle(fontSize: 25),
+            )
           ),
-          body: Stack(
-            children: [
-              Center(
-                child: Text(counter.toString(),
-                  style: TextStyle(fontSize: 25),
-                )
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        if(counter > 1) {
-                          setState(() {
-                            counter--;
-                          });
-                        } else {
-                          Fluttertoast.showToast(msg: 'End of List');
-                        }
-                        print('TAPPED LEFT');
-                      },
-                      child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width * 0.2 ,
-                        color: background.value,
-                      ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if(counter > 1) {
+                      setState(() {
+                        counter--;
+                        _currentOpacity = 0.5;
+                      });
+                    } else {
+                      Fluttertoast.showToast(msg: 'End of List');
+                    }
+                    print('TAPPED LEFT');
+                  },
+                  child: AnimatedOpacity(
+                    opacity:_currentOpacity,
+                    curve: Curves.linear,
+                    duration: const Duration(milliseconds: 500),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width * 0.2 ,
+                      color: Colors.grey,
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        if(counter < 10) {
-                          setState(() {
-                            counter++;
-                          });
-                        } else {
-                          Fluttertoast.showToast(msg: 'End of List');
-                        }
-                        print('TAPPED RIGHT');
-                      },
-                      child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width * 0.2 ,
-                        color: background.value
-                      ),
-                    )
-                  ],
+                  ),
                 ),
-              ),
-            ],
+                GestureDetector(
+                  onTap: () {
+                    if(counter < 10) {
+                      setState(() {
+                        counter++;
+                        _currentOpacity = 0.5;
+                      });
+                    } else {
+                      Fluttertoast.showToast(msg: 'End of List');
+                    }
+                    print('TAPPED RIGHT');
+                  },
+                  child: AnimatedOpacity(
+                    opacity:_currentOpacity,
+                    curve: Curves.linear,
+                    duration: const Duration(milliseconds: 500),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width * 0.2 ,
+                      color: Colors.grey
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        );
-      }
+        ],
+      ),
     );
   }
 }
