1112080453d043a747e8106fc82576a30631238f
aman-abiy <amexabiy@gmail.com>
2022-01-24 12:41:32 +0300

    added animation on tap, not working

--- lib/screens/page_demo.dart
+++ lib/screens/page_demo.dart
@@ -7,66 +7,94 @@ class PageDemo extends StatefulWidget {
   _PageDemoState createState() => _PageDemoState();
 }
 
-class _PageDemoState extends State<PageDemo> {
+class _PageDemoState extends State<PageDemo> with SingleTickerProviderStateMixin {
   int counter = 1;
+  bool isTapped = false;
+  Color onTapColor = Colors.grey;
+  late AnimationController animationController;
+  var background;
+  
 
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+
+    background = ColorTween(
+    begin: Colors.grey.shade200,
+    end: Colors.blue,
+  ).animate(animationController)
+    ..addListener(() {
+      setState(() {});
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Page Demo'),
-      ),
-      body: Stack(
-        children: [
-          Center(
-            child: Text('1',
-              style: TextStyle(fontSize: 25),
-            )
+    return AnimatedBuilder(
+      animation: animationController, 
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Page Demo'),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    if(counter > 1) {
-                      setState(() {
-                        counter--;
-                      });
-                    } else {
-                      Fluttertoast.showToast(msg: 'End of List');
-                    }
-                    print('TAPPED LEFT');
-                  },
-                  child: Container(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width * 0.2 ,
-                    color: Colors.grey,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    if(counter <= 10) {
-                      setState(() {
-                        counter++;
-                      });
-                    } else {
-                      Fluttertoast.showToast(msg: 'End of List');
-                    }
-                    print('TAPPED RIGHT');
-                  },
-                  child: Container(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width * 0.2 ,
-                    color: Colors.grey,
-                  ),
+          body: Stack(
+            children: [
+              Center(
+                child: Text(counter.toString(),
+                  style: TextStyle(fontSize: 25),
                 )
-              ],
-            ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        if(counter > 1) {
+                          setState(() {
+                            counter--;
+                          });
+                        } else {
+                          Fluttertoast.showToast(msg: 'End of List');
+                        }
+                        print('TAPPED LEFT');
+                      },
+                      child: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width * 0.2 ,
+                        color: background.value,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        if(counter < 10) {
+                          setState(() {
+                            counter++;
+                          });
+                        } else {
+                          Fluttertoast.showToast(msg: 'End of List');
+                        }
+                        print('TAPPED RIGHT');
+                      },
+                      child: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width * 0.2 ,
+                        color: background.value
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
